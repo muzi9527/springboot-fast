7601effb94ec046463e3bd529e328e2bbcd7b027
mzmoe <yuanxianzhengsb@gmail.com>
2021-01-19 11:46:49 +0800

    快速使用Maven构建SpringBoot

---
+++ src/main/java/com/muzi9527/SpringBootGOGOGO.java
@@ -0,0 +1,11 @@
+package com.muzi9527;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGOGOGO {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGOGOGO.class, args);
+    }
+}
